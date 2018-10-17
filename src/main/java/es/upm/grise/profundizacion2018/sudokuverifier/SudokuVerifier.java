048edef3161c08cc5b4ebddbfc7cc2d87b3e38e9
mirigaray <meryiri95@gmail.com>
2018-10-17 23:34:11 +0200

    SudokuVerifier and SudokuVerifierTest modified

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -43,37 +43,29 @@ public class SudokuVerifier
         return 0;
     }
     /***
-     *
-    * Method to save sudoku in matrix and to check Rule 1: A cell in a Sudoku game
-    * can only store positive digits, i.e. 1…9.
+    * Method to check Rule 1: A cell in a Sudoku game can only store positive digits, i.e. 1…9.
     */
     private void checkRule1(String candidateSolution) throws Exception {
         if (candidateSolution.length()==81){
             int aux,cont=0;
-            for (int iGridActual=0;iGridActual<3;iGridActual++){
-                for (int jGridActual=0;jGridActual<3;jGridActual++){  
-                    for (int i=0;i<3;i++){
-                        for (int j=0;j<3;j++){                       
-                            aux = Integer.parseInt(Character.toString(candidateSolution.charAt(cont)));
-                            //It has to be a number between 1 and 9, not 0.
-                            if (aux!=0)
-                                sudoku[iGridActual*3+i][jGridActual*3+j]=aux;
-                            else
-                                throw new Exception("One of the numbers is 0.");
-                            cont++;
-                        }
-                    }
+            for (int iGridActual=0;iGridActual<9;iGridActual++){
+                for (int jGridActual=0;jGridActual<9;jGridActual++){ 
+                    aux = Integer.parseInt(Character.toString(candidateSolution.charAt(cont)));
+                    if (aux!=0)
+                        sudoku[iGridActual][jGridActual]=aux;
+                    else
+                        throw new Exception("One of the numbers is 0.");
+                    cont++;
                 }
             }
-            }
+        }
         else{
             throw new Exception("Incorrect number of numbers in sudoku.");
         }
     }
 
     /***
-    * Method to check Rule 2: All digits appear only once in a sub-grid, i.e. 
-    * they cannot repeat.
+    * Method to check Rule 2: All digits appear only once in a sub-grid, i.e. they cannot repeat.
     */
     private void checkRule2() throws Exception{
         ArrayList<Integer> sortedSubgrid = new ArrayList<Integer>();
@@ -98,8 +90,7 @@ public class SudokuVerifier
     }
     
     /***
-    * Method to check Rule 3: A digit can appear only once in the rows of the 
-    * global grid.
+    * Method to check Rule 3: A digit can appear only once in the rows of the global grid.
     */
     private void checkRule3() throws Exception{
         ArrayList<Integer> sortedSubgrid = new ArrayList<Integer>();
@@ -119,8 +110,7 @@ public class SudokuVerifier
     }
     
     /***
-    * Method to check Rule 4: A digit can appear only once in the columns of the 
-    * global grid.
+    * Method to check Rule 4: A digit can appear only once in the columns of the global grid.
     */
      private void checkRule4() throws Exception{
         ArrayList<Integer> sortedSubgrid = new ArrayList<Integer>();
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -5,26 +5,25 @@ import org.junit.Test;
 
 public class SudokuVerifierTest {
         private SudokuVerifier sudVer = new SudokuVerifier();
-
+        
         /***
-         * Test to verify if the sudoku is correct(must reply 0).
+         * Test to verify if it replies 0 to a correct sudoku
          */
 	@Test
-	public void testCorrectSudoku() {
-             assertEquals(0, sudVer.verify("138426759"+
-                                            "294758163"+
-                                            "756139842"+
-                                            "591264873"+
-                                            "437819625"+
-                                            "268375914"+
-                                            "617985342"+
-                                            "942376581"+
-                                            "583421697"));
+	public void test_0() {
+             assertEquals(0, sudVer.verify( "138294756"+
+                                            "426758139"+
+                                            "759163842"+
+                                            "591437268"+
+                                            "264819375"+ 
+                                            "873625914"+
+                                            "617942583"+ 
+                                            "985376421"+
+                                            "342581697"));
         }
         
         /***
-         * Test to verify method to check Rule 1(must reply -1 to a sudoku with an
-         * incorrect length)
+         * Test to verify if it replies -1 to a sudoku with an incorrect length
          */
         @Test
 	public void testRule1_1() {
@@ -39,67 +38,80 @@ public class SudokuVerifierTest {
                                             "58"));
         }
         /***
-         * Test to verify method to check Rule 1 (must reply -1 to a sudoku with
-         * one 0)
+         * Test to verify if it replies -1 to a sudoku with one 0
          */
         public void testRule1_2() {
             assertEquals(-1, sudVer.verify("138426759"+
                                             "294758163"+
                                             "756139842"+
-                                            "591264873"+
+                                            "591064873"+
                                             "437819625"+
                                             "268375914"+
                                             "617985342"+
                                             "942376581"+
                                             "583421690"));
         }
+        /***
+         * Test to verify if it replies -1 to a sudoku with letters
+         */
+        public void testRule1_3() {
+            assertEquals(-1, sudVer.verify("138426759"+
+                                            "294758163"+
+                                            "756139842"+
+                                            "591064873"+
+                                            "437819625"+
+                                            "268375914"+
+                                            "617985342"+
+                                            "942376581"+
+                                            "5834216aa"));
+        }
         
         /***
-         * Test to verify method to check Rule 2(must reply -2 to a sudoku with 2 
-         * repeated numbers in the same sub-grid)
+         * Test to verify if it replies -2 to a sudoku with 2 repeated numbers in 
+         * the same sub-grid
          */
         @Test
 	public void testRule2() {
-             assertEquals(-2, sudVer.verify("138436759"+
-                                            "294758163"+
-                                            "756139842"+
-                                            "591264873"+
-                                            "437819625"+
-                                            "268375914"+
-                                            "617985342"+
-                                            "942376581"+
-                                            "583421697"));
+             assertEquals(-2, sudVer.verify("138294756"+
+                                            "416758139"+
+                                            "759163842"+
+                                            "591437268"+
+                                            "264819375"+ 
+                                            "873625914"+
+                                            "617942583"+ 
+                                            "985376421"+
+                                            "342581697"));
         }
         /***
-         * Test to verify method to check Rule 3(must reply -3 to a sudoku with 2
-         * repeated numbers in the same global row)
+         * Test to verify if it replies -3 to a sudoku with 2 repeated numbers in 
+         * the same global row
          */
         @Test
 	public void testRule3() {
-             assertEquals(-3, sudVer.verify("138426759"+
-                                            "194758263"+
-                                            "756139842"+
-                                            "591264873"+
-                                            "437819625"+
-                                            "268375914"+
-                                            "617985342"+
-                                            "942376581"+
-                                            "583421697"));
+             assertEquals(-3, sudVer.verify("438294756"+
+                                            "126758139"+
+                                            "759163842"+
+                                            "591437268"+
+                                            "264819375"+ 
+                                            "873625914"+
+                                            "617942583"+ 
+                                            "985376421"+
+                                            "342581697"));
         } 	
         /***
-         * Test to verify method to check Rule 4(must reply -4 to a sudoku with 2
-         * repeated numbers in the same global column)
+         * Test to verify if it replies -4 to a sudoku with 2 repeated numbers in 
+         * the same global column
          */
         @Test
-	public void testRule4() {
-            assertEquals(-4, sudVer.verify("138426759"+
-                                            "294758163"+
-                                            "756139842"+
-                                            "195264873"+
-                                            "437819625"+
-                                            "268375914"+
-                                            "617985342"+
-                                            "942376581"+
-                                            "583421697"));
+	public void test4() {
+            assertEquals(-4, sudVer.verify( "831294756"+
+                                            "426758139"+
+                                            "759163842"+
+                                            "591437268"+
+                                            "264819375"+ 
+                                            "873625914"+
+                                            "617942583"+ 
+                                            "985376421"+
+                                            "342581697"));
         }
 }
